576c02000c469f57f20be7a4d90aa4b8f977b17d
NurKhamid <khamid1196@gmail.com>
2021-04-19 23:13:26 +0700

    function locker case

---
+++ locker_case.js
@@ -0,0 +1,44 @@
+function locker_case(x){
+    floor_number = 1;
+    flag_nine = true;
+    flag_three = false;
+    flag_seven = false;
+    flag_two = false;
+    temp = [];
+    if (Number.isInteger(x)){
+        for(i = 1; i <= x; i++){
+            temp.push(i);
+            if (x==i){
+                return 'Locker is found. Go to floor ' + floor_number
+                // console.log('hi');
+            }
+            else if (flag_nine && temp.length==9){
+                floor_number+= 1
+                flag_nine = false
+                flag_three = true
+                temp = []
+            }
+            else if (flag_three && temp.length==3){
+                floor_number+= 1
+                flag_three = false
+                flag_seven = true
+                temp = []
+            }
+            else if (flag_seven && temp.length==7){
+                floor_number+= 1
+                flag_seven = false
+                flag_two = true
+                temp = []
+            }
+            else if (flag_two && temp.length==2){
+                floor_number += 1
+                flag_two = false
+                flag_nine = true
+                temp = []
+            }
+        }
+    } else {
+        return 'Please input a number'
+    }
+}
+console.log(locker_case(41));
